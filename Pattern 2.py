3e2f37dd4e1816ea4b3188728e4b814a54abe2a4
Abdullah Sher <49390045+abdullahsher445@users.noreply.github.com>
2020-07-25 21:19:21 +0500

    Add files via upload

---
+++ Pattern 2.py
@@ -0,0 +1,16 @@
+
+print("-"*50)
+print("Target")
+print("-"*50)
+print(
+"""
+   APQR
+   ABQR
+   ABCR
+   ABCD""")
+print("-"*50)
+print("Result")
+print("-"*50)
+list =["PQR","BQR","BCR","BCD"]
+for x in range(4):
+    print("A" + list[x])
